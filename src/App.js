f688aa5f22f20989c663f781877382f1d0416d3b
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-07-03 10:35:19 +0530

    create button counter react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ButtonClickCounter from './ButtonClickCounter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonClickCounter />
+    </div>
+  );
+}
+
+export default App;
